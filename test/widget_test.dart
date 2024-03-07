9e1844515981274e2aaad84f67cfd53c6d4873fe
Rabiayilmaz06 <rabiayilmazqwe@gmail.com>
2024-03-07 18:45:04 +0300

    deneme2

--- test/widget_test.dart
+++ test/widget_test.dart
@@ -7,6 +7,7 @@
 
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
+import 'package:mobil_optik_okuyucu/app.dart';
 
 import 'package:mobil_optik_okuyucu/main.dart';
 
