3e2e523a4c07e06d55833679b9e8584e1a6d47e1
Rabiayilmaz06 <rabiayilmazqwe@gmail.com>
2024-03-10 20:08:47 +0300

    buttonlar biçimlendirildi

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -9,58 +9,134 @@ class HomePage extends StatelessWidget {
         appBar: AppBar(
           title: Text('Ana Sayfa'),
         ),
+        backgroundColor: Colors.purple[50],
         body: Center(
           child: Column(
             mainAxisAlignment: MainAxisAlignment.center,
             children: [
               Padding(
                 padding: const EdgeInsets.only(bottom: 50),
-                child: Image.asset(
-                  'assets/image/logo.png',
-                  width: 200,
-                  height: 200,
+                child: ClipRRect(
+                  borderRadius: BorderRadius.circular(20),
+                  child: Image.asset(
+                    'assets/images/logo.png',
+                    width: 200,
+                    height: 200,
+                  ),
                 ),
               ),
               // Üst kısımda logo
               Container(
-                  margin: EdgeInsets.only(bottom: 10.0),
-                  child: Image.asset('assets/image/kamera.png',
-                      width: 90, height: 90)),
+                margin: EdgeInsets.only(bottom: 10.0),
+                child: Image.asset(
+                  'assets/images/kamera.png',
+                  width: 90,
+                  height: 90,
+                ),
+              ),
+
+              SizedBox(height: 20.0),
 
               // İki butonlu satır
-
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {
-                      Navigator.push(
+                  ClipRRect(
+                    borderRadius: const BorderRadius.horizontal(
+                      left: Radius.circular(50),
+                      right: Radius.circular(0),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
                           context,
                           MaterialPageRoute(
-                              builder: (context) => SinavlarPage()));
-                    },
-                    child: Text('Buton 1'),
+                              builder: (context) => SinavlarPage()),
+                        );
+                      },
+                      child: Text('Sınavlar'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFFFBE0B),
+                        onPrimary: Colors.black,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      ),
+                    ),
                   ),
                   SizedBox(width: 20.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Buton 2'),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.horizontal(
+                      left: Radius.circular(0),
+                      right: Radius.circular(50),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SinavlarPage()),
+                        );
+                      },
+                      child: Text('Sınavlar'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFFFBE0B),
+                        onPrimary: Colors.black,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      ),
+                    ),
                   ),
                 ],
               ),
+              // İkinci butonlu satırın kopyası
               SizedBox(height: 20.0),
-              // İki butonlu satır
               Row(
                 mainAxisAlignment: MainAxisAlignment.center,
                 children: [
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Buton 3'),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.horizontal(
+                      left: Radius.circular(50),
+                      right: Radius.circular(0),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SinavlarPage()),
+                        );
+                      },
+                      child: Text('Sınavlar'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFFFBE0B),
+                        onPrimary: Colors.black,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      ),
+                    ),
                   ),
                   SizedBox(width: 20.0),
-                  ElevatedButton(
-                    onPressed: () {},
-                    child: Text('Buton 4'),
+                  ClipRRect(
+                    borderRadius: const BorderRadius.horizontal(
+                      left: Radius.circular(0),
+                      right: Radius.circular(50),
+                    ),
+                    child: ElevatedButton(
+                      onPressed: () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                              builder: (context) => SinavlarPage()),
+                        );
+                      },
+                      child: Text('Sınavlar'),
+                      style: ElevatedButton.styleFrom(
+                        primary: Color(0xFFFFBE0B),
+                        onPrimary: Colors.black,
+                        padding:
+                            EdgeInsets.symmetric(horizontal: 20, vertical: 10),
+                      ),
+                    ),
                   ),
                 ],
               ),
